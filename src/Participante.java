c8fa30bd74e8cd070be8315cb15d0ccb3d456414
Gabriel Souto de Britto <20201370013@ad.ifpb.edu.br>
2022-06-08 18:36:01 -0300

    Atualização das Classes

--- src/Participante.java
+++ src/Participante.java
@@ -4,15 +4,21 @@ import preco, id;
 public class Participante {
 	private String email;
 	private String nome;
-	private id idade;
-	private ArrayList<Evento> eventos = new ArrayList<Integer>();
+	private int idade;
+	private ArrayList<Evento> eventos = new ArrayList<>();
+	
+	public Participante(String email, String nome, int idade) {
+		this.email = email;
+		this.nome = nome;
+		this.idade = idade;
+	}
 	
 	public int getPercentual() {
 		int desconto;
 		
-		if (this.idade < 18)
+		if (this.getIdade() < 18)
 			desconto = 50;
-		else if (18 <= this.idade < 60)
+		else if (18 <= this.getIdade() && this.getIdade() < 60)
 			desconto = 0;
 		else
 			desconto = 20;
@@ -27,6 +33,38 @@ public class Participante {
 	public void adicionar(ArrayList<Evento> eventos) {
 		this.eventos =  eventos.add()
 		return this.eventos
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public ArrayList<Evento> getEventos() {
+		return eventos;
+	}
+
+	public void setEventos(ArrayList<Evento> eventos) {
+		this.eventos = eventos;
 	}
 	
 }
